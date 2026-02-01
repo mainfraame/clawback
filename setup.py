0702f033cd803420ba68e0d2a90639aa3909d3ff
dayne.mentier <dayne.mentier@gmail.com>
2026-02-01 15:10:37 -0500

    chore(release): bump version to 1.0.11

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="clawback",
-    version="1.0.10",
+    version="1.0.11",
     author="Dayne",
     author_email="",
     description="Mirror congressional stock trades with automated broker execution",
